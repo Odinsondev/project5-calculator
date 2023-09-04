0c409529082e96a6b3356bf459d97bb81460eff5
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2023-09-04 02:22:33 -0700

    Feat: Add support for chaining operations
    Feat: Add function for decimal button
    
    Feat: Add functions for clear and delete buttons
    
    Fix: Numbers starting with "0"
    
    Fix: Numbers containing multiple decimals

--- javascript.js
+++ javascript.js
@@ -1,33 +1,38 @@
+
+// Global variables to be used in later functions
+
 let firstNumber = "";
 let operator = "";
 let secondNumber = "";
 let result = "";
 
+// Operation functions
+
 function add() {
   const resultAdd = Number(firstNumber) + Number(secondNumber);
-  const resultRound = Math.round((resultAdd + Number.EPSILON) * 100) / 100;
+  const resultRound = Math.round((resultAdd + Number.EPSILON) * 1000) / 1000;
   result = resultRound;
 }
 
 function subtract() {
   const resultSubtract = Number(firstNumber) - Number(secondNumber);
-  const resultRound = Math.round((resultSubtract + Number.EPSILON) * 100) / 100;
+  const resultRound = Math.round((resultSubtract + Number.EPSILON) * 1000) / 1000;
   result = resultRound;
 }
 
 function multiply() {
   const resultMultiply = Number(firstNumber) * Number(secondNumber);
-  const resultRound = Math.round((resultMultiply + Number.EPSILON) * 100) / 100;
+  const resultRound = Math.round((resultMultiply + Number.EPSILON) * 1000) / 1000;
   result = resultRound;
 }
 
-function devide() {
-  const resultDevide = Number(firstNumber) / Number(secondNumber);
-  const resultRound = Math.round((resultDevide + Number.EPSILON) * 100) / 100;
+function divide() {
+  const resultDivide = Number(firstNumber) / Number(secondNumber);
+  const resultRound = Math.round((resultDivide + Number.EPSILON) * 1000) / 1000;
   result = resultRound;
 }
 
-
+// Eventlisteners for buttons
 
 function buttons() {
   const button0 = document.getElementById('0');
@@ -40,13 +45,14 @@ function buttons() {
   const button7 = document.getElementById('7');
   const button8 = document.getElementById('8');
   const button9 = document.getElementById('9');
+  const buttonDecimal = document.getElementById('decimal');
   const buttonAdd = document.getElementById('add');
   const buttonSubtract = document.getElementById('subtract');
   const buttonMultiply = document.getElementById('multiply');
-  const buttonDevide = document.getElementById('devide');
+  const buttonDivide = document.getElementById('divide');
   const buttonOperate = document.getElementById('operate');
-
-
+  const buttonClear = document.getElementById('clear');
+  const buttonDelete = document.getElementById('delete');
 
   button0.addEventListener('click', runButton0);
   button1.addEventListener('click', runButton1);
@@ -58,29 +64,31 @@ function buttons() {
   button7.addEventListener('click', runButton7);
   button8.addEventListener('click', runButton8);
   button9.addEventListener('click', runButton9);
+  buttonDecimal.addEventListener('click', runButtonDecimal);
   buttonAdd.addEventListener('click', runButtonAdd);
   buttonSubtract.addEventListener('click', runButtonSubtract);
   buttonMultiply.addEventListener('click', runButtonMultiply);
-  buttonDevide.addEventListener('click', runButtonDevide);
+  buttonDivide.addEventListener('click', runButtonDivide);
   buttonOperate.addEventListener('click', runButtonOperate);
-
-
-
+  buttonClear.addEventListener('click', runButtonClear);
+  buttonDelete.addEventListener('click', runButtonDelete);
 }
-buttons()
+buttons();
+
+// Individual button functions
 
 function runButton0() {
-  if (operator == "" && firstNumber != "0") {   //Disables numbers starting with 00
+  if (operator == "" && firstNumber != "0") {   //Disables firstNumber starting with 00
     firstNumber = `${firstNumber}` + "0";
     updateScreen();
-  } else if (operator != "" && secondNumber != "0") {   //Disables numbers starting with 00
+  } else if (operator != "" && secondNumber != "0") {   //Disables secondNumber starting with 00
     secondNumber = `${secondNumber}` + "0";
     updateScreen3();
-  }
+  } else {}
 }
 
 function runButton1() {
-  if (firstNumber == "0") {   //Disables numbers like 01
+  if (operator == "" && firstNumber == "0") {   //Disables numbers like 01
     firstNumber = "";
   } else if (secondNumber == "0") {
     secondNumber = "";
@@ -96,7 +104,7 @@ function runButton1() {
 }
 
 function runButton2() {
-  if (firstNumber == "0") {   //Disables numbers like 01
+  if (operator == "" && firstNumber == "0") {
     firstNumber = "";
   } else if (secondNumber == "0") {
     secondNumber = "";
@@ -112,7 +120,7 @@ function runButton2() {
 }
 
 function runButton3() {
-  if (firstNumber == "0") {   //Disables numbers like 01
+  if (operator == "" && firstNumber == "0") {
     firstNumber = "";
   } else if (secondNumber == "0") {
     secondNumber = "";
@@ -128,7 +136,7 @@ function runButton3() {
 }
 
 function runButton4() {
-  if (firstNumber == "0") {   //Disables numbers like 01
+  if (operator == "" && firstNumber == "0") {
     firstNumber = "";
   } else if (secondNumber == "0") {
     secondNumber = "";
@@ -144,7 +152,7 @@ function runButton4() {
 }
 
 function runButton5() {
-  if (firstNumber == "0") {   //Disables numbers like 01
+  if (operator == "" && firstNumber == "0") {
     firstNumber = "";
   } else if (secondNumber == "0") {
     secondNumber = "";
@@ -160,7 +168,7 @@ function runButton5() {
 }
 
 function runButton6() {
-  if (firstNumber == "0") {   //Disables numbers like 01
+  if (operator == "" && firstNumber == "0") {
     firstNumber = "";
   } else if (secondNumber == "0") {
     secondNumber = "";
@@ -176,7 +184,7 @@ function runButton6() {
 }
 
 function runButton7() {
-  if (firstNumber == "0") {   //Disables numbers like 01
+  if (operator == "" && firstNumber == "0") {
     firstNumber = "";
   } else if (secondNumber == "0") {
     secondNumber = "";
@@ -192,7 +200,7 @@ function runButton7() {
 }
 
 function runButton8() {
-  if (firstNumber == "0") {   //Disables numbers like 01
+  if (operator == "" && firstNumber == "0") {
     firstNumber = "";
   } else if (secondNumber == "0") {
     secondNumber = "";
@@ -208,7 +216,7 @@ function runButton8() {
 }
 
 function runButton9() {
-  if (firstNumber == "0") {   //Disables numbers like 01
+  if (operator == "" && firstNumber == "0") {
     firstNumber = "";
   } else if (secondNumber == "0") {
     secondNumber = "";
@@ -223,43 +231,121 @@ function runButton9() {
   }
 }
 
+function runButtonDecimal() {
+  if (operator == "" && firstNumber == "00") {   //Disables numbers like 00.
+    firstNumber = "0";
+  } else if (secondNumber == "00") {
+    secondNumber = "0";
+  } else {}
+
+  if (operator == "" && !firstNumber.includes(".")) {   //Disables numbers like 0.. or 0.1.1
+    firstNumber = `${firstNumber}` + ".";
+    updateScreen();
+  } else if (operator != "" && !secondNumber.includes(".")) {
+    secondNumber = `${secondNumber}` + ".";
+    updateScreen3();
+  } else {}
+}
+
 function runButtonAdd() {
-  operator = "+";
-  updateScreen2();
+  if (operator == "") {   // Run normal code if making first calculation
+    operator = "+";
+    updateScreen2();
+  } else {   // Run if making consecutive operations without using "="
+    runButtonOperate();
+    operator = "+";   // Added operator so next input numbers would add to secondNumber
+  }                   // Not logging this operator to console
 }
 
 function runButtonSubtract() {
-  operator = "-";
-  updateScreen2();
+  if (operator == "") {
+    operator = "-";
+    updateScreen2();
+  } else {
+    runButtonOperate();
+    operator = "-";
+  }
 }
 
 function runButtonMultiply() {
-  operator = "*";
-  updateScreen2();
+  if (operator == "") {
+    operator = "\xd7";
+    updateScreen2();
+  } else {
+    runButtonOperate();
+    operator = "\xd7";
+  }
 }
 
-function runButtonDevide() {
-  operator = "/";
-  updateScreen2();
+function runButtonDivide() {
+  if (operator == "") {
+    operator = "\xf7";
+    updateScreen2();
+  } else {
+    runButtonOperate();
+    operator = "\xf7";
+  }
 }
 
 function runButtonOperate() {
   if (operator == "+") {
     add();
     updateScreenResult();
-  } else if (operator =="-") {
+    firstNumber = result.toString();   // Updates firstNumber to Result for future operations
+    operator = "";                     // Added toString to avoid potential bugs
+    secondNumber = "";
+  } else if (operator == "-") {
     subtract();
     updateScreenResult();
-  } else if (operator =="*") {
+    firstNumber = result.toString();
+    operator = "";
+    secondNumber = "";
+  } else if (operator == "\xd7") {
     multiply();
     updateScreenResult();
-  } else if (operator =="/") {
-    devide();
-    updateScreenResult();
+    firstNumber = result.toString();
+    operator = "";
+    secondNumber = "";
+  } else if (operator == "\xf7") {
+    if (secondNumber == 0) {
+      result = "58008";   // ERROR message when dividing by 0
+      updateScreenResult();
+      firstNumber = result.toString();
+      operator = "";
+      secondNumber = "";
+    } else {
+      divide();
+      updateScreenResult();
+      firstNumber = result.toString();
+      operator = "";
+      secondNumber = "";
+    }
   } else {}
+
+  console.log(firstNumber)   // Show values after operating
+  console.log(operator)
+  console.log(secondNumber)
 }
 
+function runButtonClear() {
+  firstNumber = "";
+  operator = "";
+  secondNumber = "";
+  result = "";
+  updateScreenClear();
+}
 
+function runButtonDelete() {
+  if (operator == "") {
+    firstNumber = firstNumber.slice(0, firstNumber.length -1);
+    updateScreen();
+  } else {
+    secondNumber = secondNumber.slice(0, secondNumber.length -1);
+    updateScreen3();
+  }
+}
+
+// Screen functions
 
 function updateScreen() {
   const screenBottom = document.getElementById('screen-bottom');
@@ -310,10 +396,28 @@ function updateScreenResult() {
   console.log(secondNumber)
 }
 
-//known bugs:
-//long numbers - fixed
-//numbers starting with 00 or 01 etc - fixed
+function updateScreenClear() {
+  const screenTop = document.getElementById('screen-top');
+  const topText = document.getElementById('top-text');
+  topText.textContent = "";
+  screenTop.appendChild(topText);
+
+  const screenBottom = document.getElementById('screen-bottom');
+  const bottomText = document.getElementById('bottom-text');
+  bottomText.textContent = "";
+  screenBottom.appendChild(bottomText);
+
+  console.log(firstNumber)
+  console.log(operator)
+  console.log(secondNumber)
+}
 
 //missing features:
 //function for %
-//multiple operations
+//multiple operations   -   DONE
+// . button   -   DONE
+//divide by 0   -   DONE
+//clear and del buttons   -   DONE
+
+//bugs:
+// 0 + 1 removes 0   -   DONE
